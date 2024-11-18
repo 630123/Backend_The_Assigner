3fa2069f8b4ce0266a900a2b62732d50a6197069
630123 <likhithareddy1112@gmail.com>
2024-11-18 16:28:34 +0530

    Update ContactUs.route.js

--- db/models/controllers/routes/ContactUs.route.js
+++ db/models/controllers/routes/ContactUs.route.js
@@ -5,3 +5,5 @@ const { createContact } = require('../controllers/ContactUs.controller');
 router.post('/', createContact);
 
 module.exports = router;
+PORT=5000
+DB_URI=mongodb://localhost:27017/contact_us
